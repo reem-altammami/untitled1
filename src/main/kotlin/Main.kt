2de831c14c145970b7b442f241660203de8cb19f
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:37:04 +0300

    add if

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
-const val appleNo=100.0
-const val dozen=12.0
-fun main(args: Array<String>) {
-   var noOfDozens=appleNo/dozen
-   var remainApples=appleNo%dozen
-    println(noOfDozens)
-    println(remainApples)
 
+fun main(args: Array<String>) {
+   var playerName="Reem"
+   var playerHealth=100
+     if (playerHealth==100){
+         println(playerName + " is great")
+     }else {
+         println(playerName + " is not great")
+     }
 }
