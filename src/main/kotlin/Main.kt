aca0a3642741e995a679bef342c205e835e673b9
unknown <reem.saud.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:54:21 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+const val appleNo=100.0
+const val dozen=12.0
+fun main(args: Array<String>) {
+   var noOfDozens=appleNo/dozen
+   var remainApples=appleNo%dozen
+    println(noOfDozens)
+    println(remainApples)
+
+}
